1e6516f86a50a589073d91b88c8d86b17c9c1aa3
Thomas Rutter <trr@thomasrutter.com>
2011-06-22 17:22:30 +1000

    Unfinished progress on converting db_connection to use PDO

--- db_connection.inc.php
+++ db_connection.inc.php
@@ -53,45 +53,28 @@ class db_connection
 	//  'prefix' => table name prefix of connection - optional, defaults to ''
 	//  'charset' => charset of connection (eg 'utf8') - optional, default set by mysql?
 	{
-		if (!function_exists('mysql_connect')) trigger_error('PHP does not appear to have mysql extension', E_USER_ERROR);
+		if (!class_exists('PDO')) throw new Exception('PHP does not appear to have PDO extension enabled');
 		
 		$this->num_queries = 0;
 		$this->prefix = isset($dbsettings['prefix']) ? $dbsettings['prefix'] : '';
-		
-		if(!empty($dbsettings['persistent']))
-			$this->connection = 
-				@mysql_pconnect($dbsettings['server'], $dbsettings['user'], $dbsettings['pass']);
-		else
-			$this->connection = 
-				@mysql_connect($dbsettings['server'], $dbsettings['user'], $dbsettings['pass']);
+
+		$opts = array();
+		if (isset($dbsettings['database'])) $opts[] = "dbname=" . $dbsettings['database'];
+		if (isset($dbsettings['server'])) $opts[] = "host=" . $dbsettings['server'];
+		if (isset($dbsettings['charset'])) $opts[] = "charset=" . $dbsettings['charset'];
+
+		$dsn = empty($dbsettings['dsn']) ? 
+			("mysql:" . implode(';', $opts)) :
+			($dbsettings['dsn'] . ($opts ? ";".implode(';',$opts) : ''));
+
+		$this->connection = new PDO(
+			$dsn,
+			isset($dbsettings['user']) ? $dbsettings['user'] : null,
+			isset($dbsettings['pass']) ? $dbsettings['pass'] : null);
+		}
 
 		if (!$this->connection)
-			return $this->set_error('Database connection failed', false);
-		
-		if (!empty($dbsettings['database']))
-		{
-			$dbselect = mysql_select_db($dbsettings['database'], $this->connection);
-			if(!$dbselect)
-			{
-				mysql_close($this->connection);
-				$this->connection = false;
-				return $this->set_error('Database selection failed', false);
-			}
-		}
-		
-		if (!empty($dbsettings['charset']))
-		{
-			$result = function_exists('mysql_set_charset')
-				? mysql_set_charset($dbsettings['charset'], $this->connection)
-				: mysql_query('SET NAMES \'' . $this->escape($dbsettings['charset']) . '\'', $this->connection);
-
-			if (!$result)
-			{
-				mysql_close($this->connection);
-				$this->connection = false;
-				return $this->set_error('Connection character set selection failed, maybe not supported', false);
-			}
-		}
+			throw new Exception('Failed to connect to database');
 	}
 	
 	public function close()
@@ -99,12 +82,11 @@ class db_connection
 	// returns false if there was no connection, true otherwise
 	{
 		if (!$this->connection)
-			return $this->set_error('Could not close connection; connection not open', false);
+			return false;
+			
+		// todo free result resources? if (is_resource($this->result)) @mysql_free_result($this->result);
 
-		if (is_resource($this->result)) @mysql_free_result($this->result);
-		mysql_close($this->connection);
-		$this->connection = false;
-		$this->result = null;
+		$this->connection = null;
 		return true;
 	}
 	
@@ -118,14 +100,14 @@ class db_connection
 	// escaping in this way, instead of addslashes(), is important if using 
 	// multi-byte charsets (other than utf-8 which is safe)
 	{ 
-		return mysql_real_escape_string($string, $this->connection);
+		return $this->connection->real_escape_string($string);
 	}
 	
 	public function query($query)
 	// tries a query.	returns false if unsuccessful, or result resource if successful
 	{
 		if (!$this->connection)
-			return $this->set_error('Database query failed; database connection not open');
+			throw new Exception('Database query failed; database connection not open');
 
 		$this->num_queries++;
 		if (DEBUG)
@@ -133,6 +115,25 @@ class db_connection
 			$debug = &debug::getinstance();
 			$taskid = $debug->starttask('db_connection', 'Database query', $this->describe_query($query));
 		}
+		$argcount = func_num_args();
+		if ($argcount > 1) {
+			$stmt = $this->connection->prepare($query);
+			$format = '';
+			$params = array_slice(func_get_args(), 1);
+			foreach ($params as $id => $param) {
+				$format .= is_string($param) ? 's' :
+					(is_int($param) ? 'i' :
+					(is_float($param) ? 'd' :
+					(is_bool($param) ? 'i' :
+					's')));
+			}
+			$stmt->bind_param($format, $params);
+			
+
+		}
+
+
+
 		$this->result = mysql_query($query, $this->connection);
 		if (!$this->result) $this->set_error('Database query failed; no result was returned');
 		if (DEBUG) $debug->endtask($taskid);
